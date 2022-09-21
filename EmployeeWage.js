2f1cb662215815377ded9e32680f379509d7083b
ranirban60 <ranirban60@gmail.com>
2022-09-21 18:59:37 +0530

    Added: Calculate wages for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,17 +6,26 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
+const WORKING_DAYS = 20;
 
-let empType = Math.floor(Math.random() * 3);
-function getWorkingHrs(empType) {
-    switch (empType) {
-      case IS_PART_TIME: return PART_TIME_HRS;
-      case IS_FULL_TIME: return FULL_TIME_HRS;
-      default: return 0;
-    }
+function calcWagesForAMonth(WORKING_DAYS) {
+  let empHrs = 0;
+  let d=0;
+  while(d<WORKING_DAYS){
+    let empType = Math.floor(Math.random() * 3);
+    empHrs+=getWorkingHrs(empType);
+    d++;
   }
-  
-  let empHrs = getWorkingHrs(empType);
-  console.log("Emp Hours: " + empHrs);
-  let empWage = empHrs * WAGE_PER_HR;
-  console.log("Emp Wage: " + empWage);
+  let empWageForMonth = empHrs * WAGE_PER_HR;
+  return empWageForMonth;
+}
+
+function getWorkingHrs(empType) {
+  switch (empType) {
+    case IS_PART_TIME: return PART_TIME_HRS;
+    case IS_FULL_TIME: return FULL_TIME_HRS;
+    default: return 0;
+  }
+}
+
+console.log("Total Wage per a month : " + calcWagesForAMonth(WORKING_DAYS));
