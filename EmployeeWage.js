c82364d6e2641527ce8c032ae417ab3d5a802733
ranirban60 <ranirban60@gmail.com>
2022-09-21 19:28:30 +0530

    Added: Calculate wagestill the condition is reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,28 +9,26 @@ const WAGE_PER_HR = 20;
 const WORKING_DAYS = 20;
 
 function calcWagesForAMonth(WORKING_DAYS) {
-    let empHrs = 0;
-    let d=0;
-    while(d<WORKING_DAYS){
-      let empType = Math.floor(Math.random() * 3);
-      empHrs+=getWorkingHrs(empType);
-      d++;
-    }
-    let empWageForMonth = empHrs * WAGE_PER_HR;
-    return empWageForMonth;
+  let empHrs = 0;
+  let days=0;
+  while(days<WORKING_DAYS && empHrs<=160){
+    let empType = Math.floor(Math.random() * 3);
+    empHrs+=getWorkingHrs(empType);
+    days++;
   }
+  let empWageForMonth = empHrs * WAGE_PER_HR;
+  console.log(`    Total working days = ${days} 
+    Total Hrs = ${empHrs }
+    Total Wage for Month = ${empWageForMonth}`)
+  return empWageForMonth;
+}
 
 function getWorkingHrs(empType) {
-    switch (empType) {
-      case IS_PART_TIME:
-         return PART_TIME_HRS;
-      case IS_FULL_TIME:
-         return FULL_TIME_HRS;
-      default: 
-         return 0;
-    }
+  switch (empType) {
+    case IS_PART_TIME: return PART_TIME_HRS;
+    case IS_FULL_TIME: return FULL_TIME_HRS;
+    default: return 0;
   }
-  
-  console.log("Total Wage per a month : " + calcWagesForAMonth(WORKING_DAYS));
+}
 
-
+calcWagesForAMonth(WORKING_DAYS);
