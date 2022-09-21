c648a33645e6f95220e34105a581704ad871fdab
ranirban60 <ranirban60@gmail.com>
2022-09-21 16:44:29 +0530

    Added: Ability to check employee attendance

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,10 @@
 console.log("Welcome to Employee wage problem")
+const IS_ABSENT = 0;
+
+let attendance = Math.floor(Math.random() * 2);
+if (attendance == IS_ABSENT) {
+  console.log('Employee is absent');
+  return;
+}else{
+  console.log('Employee is present');
+}
